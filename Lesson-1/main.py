783f4b4896672cda678b984a6947278ea6e82445
Vaibhav Singhal <web1985@gmail.com>
2017-07-03 13:27:55 +0530

    resolved error in main.py

--- Lesson-1/main.py
+++ Lesson-1/main.py
@@ -5,8 +5,6 @@ from platform import python_version
 if __name__ == "__main__":
     if 3/2 != 1.5:
         print('You are using Python:', python_version() , ', Your code wont work here!!')
-        sys.exit(1)
     else:
         print("You are using Python version:", python_version(), "Welcome!!")
-	sys.exit(1)
-
+sys.exit(1)
